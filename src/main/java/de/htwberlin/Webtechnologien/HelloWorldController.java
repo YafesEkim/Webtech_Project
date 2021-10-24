aa8df2f9a474d89cb6746c7e1955aa7a035fa942
YafesEkim <yafes_ekim@hotmail.de>
2021-10-24 15:19:22 +0200

    1. Milestone

---
+++ src/main/java/de/htwberlin/Webtechnologien/HelloWorldController.java
@@ -0,0 +1,22 @@
+package de.htwberlin.Webtechnologien;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+/*    @GetMapping(path = "/")
+    public String showHelloWorldPage(Model model){
+        model.addAttribute("helloName","Hello Yafes");
+        return "HelloWorld"; //html Datei Name als Return --> falls in einem weiteren Ordner --> "Ordnername/Filename"
+    }*/
+
+    @GetMapping(path = "/")
+    public ModelAndView showHelloWorld() {
+        return new ModelAndView("HelloWorld");
+    }
+
+}
