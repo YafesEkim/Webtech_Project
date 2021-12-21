7f14fb685a9f9b1170e3d89af3fdad4ec127893e
YafesEkim <yafes_ekim@hotmail.de>
2021-12-21 17:44:31 +0100

    Changes

--- src/main/java/de/htwberlin/Webtechnologien/config/WebConfig.java
+++ src/main/java/de/htwberlin/Webtechnologien/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
        registry.addMapping("api/**").allowedMethods("*").allowedOrigins(
-               "http://localhost:3000",
-                "https://webtech-frontend-accounting.herokuapp.com");
+               "http://localhost:3000/",
+                "https://webtech-frontend-accounting.herokuapp.com/");
     }
 }
